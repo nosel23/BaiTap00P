b4101082913ec8e3db67fa9deb715cd1c7b39c0b
Son <lson5806@gmail.com>
2024-11-13 00:56:56 +0700

    add

---
+++ bai2/Test.java
@@ -0,0 +1,8 @@
+package bai2;
+
+public class Test {
+	public static void main(String[] args) {
+        DiaChi diaChi = new DiaChi("10", "Xuan Thien", "Tho Xuan", "Thanh Hoa");
+        System.out.println(diaChi.toString());
+    }
+}
